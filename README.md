d710e5dc53d001e0d75cd02aea48e67ec9a56b80
lucas-aquino <luckasakino@protonmail.com>
2023-07-17 12:43:11 -0300

    Se termino la seccion styled component del curso

---
+++ README.md
@@ -0,0 +1,1 @@
+Styled Component Curso HolaMundo
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { styled, keyframes }  from 'styled-components'
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+
+const Button = styled.button`
+  transition: box-shadow 0.3s ease;
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+
+  &:hover {
+    box-shadow: 1px 2px 5px rgb(0, 0, 0, 0.8);
+  }
+`
+
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`
+
+const Link = ({ className, ...props }) => {
+  return <a className={className} {...props}></a>
+}
+
+const StyledLink = styled(Link)`
+  color: blue;
+  cursor: pointer;
+`
+
+const Input = styled.input.attrs(props => ({
+  type: 'text',
+  color: props.color || 'red'
+}))`
+  font-size: 20px;
+  border: 1px solid red;
+  color: ${props => props.color};
+`
+
+const girar = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Password = styled(Input).attrs({
+  type: 'password'
+})`
+
+`
+
+const Rotar = styled.div`
+  display: inline-block;
+  animation: ${girar} 1s linear infinite;
+  font-size: 28px;
+`
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <P>Hola</P>
+      <Button>Enviar</Button>
+      <Button primary>Enviar</Button>
+      <BlockButton>Enviar</BlockButton>
+      <BlockButton primary>Enviar</BlockButton>
+      <BlockButton primary as='a' href='#'>Enviar</BlockButton>
+      <StyledLink href='#'>Link con estilo 😎</StyledLink>
+      <Input color='blue'></Input>
+      <Password></Password>
+      <br />
+      <Rotar>🥴</Rotar>
+    </div>
   )
 }
 
